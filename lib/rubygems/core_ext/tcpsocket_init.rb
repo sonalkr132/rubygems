da29c537b77cb7dad457e054d1f7cf88681ec222
Aditya Prakash <aditya.prakash132@gmail.com>
2019-03-11 10:14:54 +0530

    some

--- lib/rubygems/core_ext/tcpsocket_init.rb
+++ lib/rubygems/core_ext/tcpsocket_init.rb
@@ -10,33 +10,47 @@ module CoreExtensions
       CONNECTION_TIMEOUT = 5
 
       def initialize(host, serv, *rest)
+        conns = []
+        @threads = []
+
         mutex = Mutex.new
-        addrs = []
+        errors = Queue.new
         cond_var = ConditionVariable.new
 
+        Thread.report_on_exception = false if defined? Thread.report_on_exception = ()
+
         Addrinfo.foreach(host, serv, nil, :STREAM) do |addr|
-          Thread.report_on_exception = false if defined? Thread.report_on_exception = ()
-          Thread.new(addr) do
-            # raises Errno::ECONNREFUSED when ip:port is unreachable
-            Socket.tcp(addr.ip_address, serv).close
-            mutex.synchronize do
-              addrs << addr.ip_address
-              cond_var.signal
+          @threads << Thread.new(addr) do
+            begin
+              conn = super(addr.ip_address, serv, *rest)
+
+              mutex.synchronize do
+                unless conn.closed? || conn.nil?
+                  conns << conn
+                  cond_var.signal
+                end
+              end
+            rescue Exception => e
+              mutex.synchronize do
+                errors << e
+              end
             end
           end
         end
 
         mutex.synchronize do
           timeout_time = CONNECTION_TIMEOUT + Time.now.to_f
-          while addrs.empty? && (remaining_time = timeout_time - Time.now.to_f) > 0
+          while conns.empty? && (remaining_time = timeout_time - Time.now.to_f) > 0
             cond_var.wait(mutex, remaining_time)
           end
 
-          host = addrs.shift unless addrs.empty?
+          raise errors.pop if conns.empty?
         end
 
-        super(host, serv, *rest)
+        @threads.each(&:join)
+        conns.shift
       end
+
     end
   end
 end
